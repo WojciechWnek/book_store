5927f0f86f37c1cd3da8eeb704126eee0690dfd3
Wojciech Wnek <wojciech.wnek97@gmail.com>
2023-07-07 14:03:15 +0200

    Add Book author relation to admin

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Book
+from .models import Book, Author
 
 
 # Customize admin panel
@@ -9,5 +9,9 @@ class BookAdmin(admin.ModelAdmin):
     list_filter= ("author","rating")
     list_display = ("title", "author")
 
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display=("first_name", "last_name")
+
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
